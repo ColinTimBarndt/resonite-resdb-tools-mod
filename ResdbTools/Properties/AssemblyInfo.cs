29332cceb7223be82f6e72750d00aa4ed6cc4d84
Colin Tim Barndt <contact@colintimbarndt.de>
2026-01-15 07:04:50 +0100

    Update assembly description

--- ResdbTools/Properties/AssemblyInfo.cs
+++ ResdbTools/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("ResdbTools")]
 [assembly: AssemblyProduct("ResdbTools")]
-[assembly: AssemblyDescription("Smarter generic type suggestions for component selectors.")]
+[assembly: AssemblyDescription("Extends the inventory browser with tools to get and modify Resonite database records.")]
 [assembly: AssemblyCompany("Colin Tim Barndt")]
 [assembly: AssemblyCopyright("Copyright © 2026 Colin Tim Barndt")]
 [assembly: AssemblyVersion(ResdbTools.ResdbTools.VERSION_CONSTANT)]
